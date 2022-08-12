3d39ccdfba25f26432b982dc297fef8aa026b503
Timothy Efraim Hotasi <timothyefraim35@gmail.com>
2022-08-12 18:58:24 +0700

    Debug #14 ssh

--- playground/settings.py
+++ playground/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@mc32=!2m8=%f5^_n@#8i)87i4ij+pokqeain@+8bej00nez6@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['130.211.220.19']
+ALLOWED_HOSTS = ['103.255.156.104']
 
 
 # Application definition
